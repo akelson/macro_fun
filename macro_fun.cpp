acd605336f0ab0ba44e1306fcf9ac954992cba98
Adam Kelson <adam@adamkelson.com>
2024-06-19 22:34:49 -0600

    Add unnecessary semicolons to make things look better.

--- macro_fun.cpp
+++ macro_fun.cpp
@@ -40,16 +40,16 @@ struct A
 };
 
 #define STRUCT(...) DISPLAYABLE(__VA_ARGS__)
-STRUCT(A_1, ival_1)
-STRUCT(A_2, fval_1, ival_2, a_1)
-STRUCT(A, ival, bval, a_1_1, a_1_2, a_2)
+STRUCT(A_1, ival_1);
+STRUCT(A_2, fval_1, ival_2, a_1);
+STRUCT(A, ival, bval, a_1_1, a_1_2, a_2);
 #undef STRUCT
 
 // The same set of STRUCT() calls, perhaps stored in a .def, could be used multiple times.
 #define STRUCT(...)
-STRUCT(A_1, ival_1)
-STRUCT(A_2, fval_1, ival_2, a_1)
-STRUCT(A, ival, bval, a_1_1, a_1_2, a_2)
+STRUCT(A_1, ival_1);
+STRUCT(A_2, fval_1, ival_2, a_1);
+STRUCT(A, ival, bval, a_1_1, a_1_2, a_2);
 #undef STRUCT
 
 int main()
